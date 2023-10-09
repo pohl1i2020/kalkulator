a6ce8166a35a62814f695fb578f683335f333542
pohl1i2020 <102365236+pohl1i2020@users.noreply.github.com>
2023-10-09 10:30:00 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+const firstNumber = document.querySelector(#firstNumber)
+const secondNumber = document.querySelector(#secondNumber)
+const znak = document.querySelector(#znak)
+const result = document.querySelector(#result)
+const submit = document.querySelector(#buton)
+
+
+function obliczanie(){
+    let firstNumber = parseInt(firstNumber.value)
+    let secondNumber = parseInt(secondNumber.value)
+    
+
+
+}
